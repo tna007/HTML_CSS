316295e7869de754e4c162cfb90d9bdb61f3698f
tna007 <anh.to@edu.bc.fi>
2021-03-21 15:01:08 +0200

    add to top button
    animated hero text
    
    add social media icons
    
    add logo

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
---> [My very first portfolio]('https://tna007.github.io/HTML_CSS/portfolio-project/index.html') <--
+--> [My very first portfolio](https://tna007.github.io/HTML_CSS/portfolio-project/index.html) <--
--- portfolio-project/script.js
+++ portfolio-project/script.js
@@ -1,11 +1,23 @@
 let burgerMenu = document.getElementById("burger");
 let nav = document.querySelector(".links");
+let toTopBtn = document.querySelector(".toTop");
 
 burgerMenu.addEventListener("click", () => {
   event.preventDefault();
   nav.classList.toggle("active");
 });
 
+const toTop = () => {
+  if (
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
+  ) {
+    toTopBtn.style.bottom = "20px";
+  } else {
+    toTopBtn.style.bottom = "-50px";
+  }
+};
+document.addEventListener("scroll", toTop);
 /*window.onscroll = function () {
   scrollFunction();
 };
